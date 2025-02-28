9d18dfbec8983884b0965aeeda0620eed88e0b3d
Dilpreet Singh <dilpreet1851990@gmail.com>
2025-02-28 11:01:00 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // typing animation
 
 var typed = new Typed('.typing',{
-    strings:["","Front-end Developer","Web Developer","JavaScript Developer","Angular Developer"],
+    strings:["","Front-end Developer","Web Developer","JavaScript Developer","React Developer"],
     typedSpeed:100,
     backSpeed:60,
     loop:true
